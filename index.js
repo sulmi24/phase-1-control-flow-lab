fbd68865a6c2d425f8bd168fcaffbc51e21c6feb
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 19:44:08 +0200

    control flow is done

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(freeValue) {
   // Write your code here!
+  if (freeValue <= 400) {
+    return "This one is on me!";
+  } else if (freeValue > 400 && freeValue < 2000) {
+    return "That will be twenty bucks.";
+  } else if (freeValue > 2000 && freeValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+ternaryCheckCity("NYC");
+
+function switchOnCharmFromTip(tip) {
+  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip("generous");
